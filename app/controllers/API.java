7c6fe3103e2e107cc17845685df78f7f05b14202
mpak_ <mpakunderscore@gmail.com>
2014-04-07 05:08:31 +0400

    Jsoup and pre API for words[]

--- app/controllers/API.java
+++ app/controllers/API.java
@@ -5,9 +5,14 @@ import com.fasterxml.jackson.databind.JsonNode;
 import models.Domain;
 import models.UserData;
 import models.WebData;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import play.cache.Cache;
 import play.mvc.Controller;
 import play.mvc.Result;
+import scala.Int;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -16,10 +21,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static play.libs.Json.toJson;
 
@@ -30,27 +32,32 @@ public class API extends Controller {
 
     private static final String USER_AGENT = "Mozilla/5.0";
 
-    public static Result add() throws IOException { //String url
-
-        //TODO GET
-        String url = request().body().asJson().get("url").asText();
+    public static Result add(String url) throws IOException { //TODO actually Cache is not needed here.
 
         int userId = 0;
         if (session("userId") != null)
             userId = Integer.parseInt(session("userId"));
 
+        WebData webData = Ebean.find(WebData.class).where().eq("url", url).findUnique();
 
-        UserData userData = (UserData) Cache.get("UserData" + userId + url);
-        if (userData == null) {
+        if (webData == null) {
 
-            WebData webData = (WebData) Cache.get("WebData" + url);
-            if (webData == null) {
+            webData = requestWebData(url);
+            webData.save();
 
-                webData = requestWebData(url);
-                webData.save(); // will generate id? use update or get instead
+            String domainString = webData.getDomainString();
 
-                Cache.set("WebData" + url, webData);
+            Domain domain = Ebean.find(Domain.class).where().idEq(domainString).findUnique();
+            if (domain == null) {
+
+                domain = new Domain(webData.getDomainString(), false);
+                domain.save();
             }
+        }
+
+        UserData userData = Ebean.find(UserData.class).where().eq("userId", userId).eq("webDataId", webData.getId()).findUnique();
+
+        if (userData == null) {
 
             userData = new UserData(userId, webData.getId());
             userData.save();
@@ -61,50 +68,62 @@ public class API extends Controller {
             userData.update();
         }
 
-        Cache.set("UserData" + userId + url, userData);
-
-        return ok();
+        return ok(toJson(webData)); //TODO return Events (each notification should be generated on server) and WebData (for popup.html)
     }
 
     private static WebData requestWebData(String url) throws IOException {
 
-        //TODO get data from url request, not from extension
-        JsonNode body = request().body().asJson();
-        String title = body.get("title").asText();
-        String tags = body.get("tags").asText(); //as json
+//        URL obj = new URL(url);
+//        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+//
+//        con.setRequestMethod("GET");
+//
+//        con.setRequestProperty("User-Agent", USER_AGENT);
+//
+//        int responseCode = con.getResponseCode();
+//
+//        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+//        String inputLine;
+//
+//        StringBuffer response = new StringBuffer();
+//
+//        while ((inputLine = in.readLine()) != null) {
+//            response.append(inputLine);
+//        }
+//        in.close();
+
+        Document doc = Jsoup.connect(url).get();
 
         Map<String, Integer> words = new HashMap<String, Integer>();
 
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        String text = doc.body().text();
 
-        con.setRequestMethod("GET");
+        Map<String, Integer> tagsMap = new HashMap<String, Integer>();
 
-        con.setRequestProperty("User-Agent", USER_AGENT);
+        String tags = String.valueOf(toJson(tagsMap));
 
-        int responseCode = con.getResponseCode();
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
 
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
+
+        int uniqueWordsCount = words.size();
+
+        int wordsCount = 0;
+        for (int value : words.values()) {
+            wordsCount += value;
         }
-        in.close();
 
-        //TODO org.jsoup or Regex ?
-        //titleRegex = <title>(.+)<\/title>
-        //wordsStrings <.+>this<.+> without
+        String title = doc.title();
 
-        return new WebData(url, title, tags, 0, 0);
+//        String favIconUrl = doc.select("link[href~=.*\\.ico]").first().attr("href");
+
+//        String favIconUrl = WebData.getDomainString(url) + "/favicon.ico";
+
+        return new WebData(url, title, tags, wordsCount, uniqueWordsCount);
     }
 
-    public static Result getApproved() {
+    public static Result getApprovedDomains() {
 
-        if (session("connected") == null) return ok();
-
-        List<Domain> domains = Ebean.find(Domain.class).where().eq("isApproved", true).findList();
+        List<Domain> domains = Ebean.find(Domain.class).where().eq("isApproved", Boolean.TRUE).findList();
 
         List<String> result = new ArrayList<String>();
         for (Domain domain: domains) {
@@ -113,5 +132,4 @@ public class API extends Controller {
 
         return ok(toJson(result));
     }
-
 }
