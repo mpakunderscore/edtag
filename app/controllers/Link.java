a4881505b18a98c16b24d7f6551c3c1e022ac92d
mpak_ <mpakunderscore@gmail.com>
2014-05-20 01:15:24 +0400

    link controller

---
+++ app/controllers/Link.java
@@ -0,0 +1,16 @@
+package controllers;
+
+//import models.Client;
+import com.avaje.ebean.Ebean;
+import models.WebData;
+import play.mvc.*;
+import views.html.index;
+
+import static play.libs.Json.toJson;
+
+public class Link extends Controller {
+
+    public static Result show(int id) {
+        return ok(toJson(Ebean.find(WebData.class).where().eq("id", id).findUnique()));
+    }
+}
