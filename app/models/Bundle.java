c7e52e68a400496c8c9c976bfec4361438fcfff1
mpak_ <mpakunderscore@gmail.com>
2014-06-30 22:57:12 +0400

    Fix

--- app/models/Bundle.java
+++ app/models/Bundle.java
@@ -77,4 +77,20 @@ public class Bundle extends Model {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 }
--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,6 +1,25 @@
 # --- !Ups
-ALTER TABLE course ADD COLUMN description VARCHAR(255)
+
+create table bundle (
+  id                        bigint not null,
+  user_id                   integer,
+  description               varchar(255),
+  title                     varchar(255),
+  web_data_ids              varchar(255),
+  tags                      TEXT,
+  constraint pk_course primary key (id))
+;
+
+create sequence bundle_seq;
+
 
 # --- !Downs
-ALTER TABLE course DROP description
+
+drop table if exists course cascade;
+
+drop sequence if exists course_seq;
+
+drop table if exists bundle cascade;
+
+drop sequence if exists bundle_seq;
 
