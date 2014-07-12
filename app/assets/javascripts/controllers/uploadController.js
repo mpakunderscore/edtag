df83605704563fda2e44306f828312636bd62d23
gleb-svechnikov <glebsvechnikov@iMacs.local>
2014-07-13 03:05:00 +0400

    Different UI for bundle generation

--- app/assets/javascripts/controllers/uploadController.js
+++ app/assets/javascripts/controllers/uploadController.js
@@ -1,36 +1,57 @@
 edTagApp.controller('uploadController', function($scope, fileReader, $http) {
      console.log(fileReader)
+
     $scope.bundle = {
-        name: 'Test bundle',
+        name: 'Search engines',
         links: [
             {url: "http://ya.ru"},
+            {url: "http://gogle.com"},
+            {url: "http://duckduckgo.com"}
         ],
         img: null
 
-    }
+    };
+
     $scope.addLink = function(){
-        $scope.bundle.links.push({url : ""});
+
+        var count = $scope.bundle.links.length,
+            valid = 0;
+
+        $scope.bundle.links.forEach(function(value){
+            if (value.url){
+                 valid++;
+            }
+        });
+
+        if (valid == count){
+            $scope.bundle.links.push({url:''}); // This should just add an empty input
+                                   // For some reason it copies the last typed input
+            // If you change this to .push(null),
+            // it will work for the first change(), but not thereafter
+        }
+
+
     }
+
+
+
     $scope.getFile = function () {
-        $scope.progress = 0;
         fileReader.readAsDataUrl($scope.file, $scope).then(function(result) {
             $scope.imageSrc = result;
         });
     };
 
     $scope.submitBundle = function(){
-            $http({
-                url: '/api/bundle/add',
-                method: "POST",
-                data: $scope.bundle
+        $http.post('/api/bundle/add',$scope.bundle,{'Content-Type': 'multipart/form-data'})
+            .success(function(data, status, headers, config) {
+              // this callback will be called asynchronously
+              // when the response is available
             })
-            .then(function(response) {
-                    console.log('success', response)
-                },
-                function(response) { // optional
-                    console.log('failure', response)
-                }
-            );
+            .error(function(data, status, headers, config) {
+              // called asynchronously if an error occurs
+              // or server returns response with an error status.
+            });
+
     };
 
 });
